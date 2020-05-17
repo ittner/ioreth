10d92a1f909509eca4f74d851863d6f4ceb9d0a1
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2020-05-17 13:17:53 -0300

    Init repository
    Mostly working version squash-merged from development commits.
    Needs a clean-up, but connects, replies to messages and publishes
    bulletins.

---
+++ aprsbot.py
@@ -0,0 +1,362 @@
+#!/usr/bin/env python3
+
+
+import socket
+import select
+import sys
+import time
+import logging
+import configparser
+import os
+import re
+
+import ax25
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)-15s %(levelname)s: %(funcName)s %(message)s"
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class TcpKissHandler:
+    FEND = b"\xc0"
+    FESC = b"\xdb"
+    TFEND = b"\xdc"
+    TFESC = b"\xdd"
+    DATA = b"\x00"
+    FESC_TFESC = FESC + TFESC
+    FESC_TFEND = FESC + TFEND
+
+    def __init__(self, addr, port):
+        self._addr = addr
+        self._port = port
+        self._sock = None
+        self._inbuf = None
+        self._outbuf = None
+        self._run = False
+
+    def connect(self):
+        if self._sock:
+            self.disconnect()
+        self._inbuf = bytearray()
+        self._outbuf = bytearray()
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.connect((self._addr, self._port))
+        self.on_connect()
+
+    def disconnect(self):
+        if self._sock:
+            self._sock.close()
+            self._sock = None
+            self._inbuf = None
+            self._outbuf = None
+            self.on_disconnect()
+        self.exit_loop()
+
+    def loop(self):
+
+        poller = select.poll()
+        fd = self._sock.fileno()
+        self._run = True
+        disconnected = False
+
+        while self._run:
+            flags = select.POLLIN | select.POLLHUP | select.POLLERR
+            if len(self._outbuf) > 0:
+                flags |= select.POLLOUT
+            poller.register(fd, flags)
+            events = poller.poll(1000)
+
+            # There is only one :/
+            for _, evt in events:
+                if evt & (select.POLLHUP | select.POLLERR):
+                    disconnected = True
+
+                if evt & select.POLLIN:
+                    rdata = self._sock.recv(2048)
+                    if len(rdata) == 0:
+                        disconnected = True
+                    else:
+                        self._inbuf += rdata
+
+                if evt & select.POLLOUT:
+                    nsent = self._sock.send(self._outbuf)
+                    self._outbuf = self._outbuf[nsent:]
+
+            poller.unregister(fd)
+
+            while len(self._inbuf) > 3:
+                # FEND, FDATA, escaped_data, FEND, ...
+                if self._inbuf[0] != ord(TcpKissHandler.FEND):
+                    raise ValueError("Bad frame start")
+                lst = self._inbuf[2:].split(TcpKissHandler.FEND, 1)
+                if len(lst) > 1:
+                    self._inbuf = lst[1]
+                    frame = (
+                        lst[0]
+                        .replace(TcpKissHandler.FESC_TFEND, TcpKissHandler.FEND)
+                        .replace(TcpKissHandler.FESC_TFESC, TcpKissHandler.FESC)
+                    )
+                    self.on_recv(frame)
+
+            self.on_loop_hook()
+
+            if disconnected:
+                self.disconnect()
+
+    def exit_loop(self):
+        self._run = False
+
+    def write_frame(self, frame_bytes):
+        """Send a complete frame."""
+        esc_frame = frame_bytes.replace(
+            TcpKissHandler.FESC, TcpKissHandler.FESC_TFESC
+        ).replace(TcpKissHandler.FEND, TcpKissHandler.FESC_TFEND)
+        self._outbuf += (
+            TcpKissHandler.FEND + TcpKissHandler.DATA + esc_frame + TcpKissHandler.FEND
+        )
+
+    def on_connect(self):
+        pass
+
+    def on_recv(self, frame_bytes):
+        pass
+
+    def on_disconnect(self):
+        pass
+
+    def on_loop_hook(self):
+        pass
+
+
+class KissPrint(TcpKissHandler):
+    def on_connect(self):
+        logger.info("Connected!")
+
+    def on_recv(self, frame_bytes):
+        try:
+            f = ax25.Frame.from_kiss_bytes(frame_bytes)
+            print(f)
+        except Exception as exc:
+            logger.warning(exc)
+
+    def on_disconnect(self):
+        logger.info("Disconnected!")
+        self.connect()
+
+
+class AprsBot(TcpKissHandler):
+    DEFAULT_PATH = ["WIDE1-1", "WIDE2-2"]
+    DEFAULT_DESTINATION = "APRS"
+
+    def __init__(self, callsign, host, port):
+        TcpKissHandler.__init__(self, host, port)
+        self.callsign = callsign
+        self._snd_queue = []
+        self._snd_queue_interval = 2
+        self._snd_queue_last = time.monotonic()
+        self._base_frame = ax25.Frame(
+            ax25.Address.from_string(self.callsign),
+            ax25.Address.from_string(AprsBot.DEFAULT_DESTINATION),
+            [ax25.Address.from_string(s) for s in AprsBot.DEFAULT_PATH],
+            ax25.APRS_CONTROL_FLD,
+            ax25.APRS_PROTOCOL_ID,
+            b"",
+        )
+
+    def on_recv(self, frame_bytes):
+        try:
+            frame = ax25.Frame.from_kiss_bytes(frame_bytes)
+            logger.info("RECV: %s", str(frame))
+            self.on_recv_frame(frame)
+        except Exception as exc:
+            logger.warning(exc)
+
+    def on_recv_frame(self, frame):
+        pass
+
+    def send_aprs_data(self, data_bytes):
+        try:
+            self._base_frame.info = data_bytes
+            logger.info("SEND: %s", str(self._base_frame))
+            self.write_frame(self._base_frame.to_kiss_bytes())
+        except Exception as exc:
+            logger.warning(exc)
+
+    def enqueue_aprs_data(self, data_bytes):
+        logger.info("APRS message enqueued for sending")
+        self._snd_queue.append(data_bytes)
+
+    def _dequeue_aprs(self):
+        now = time.monotonic()
+        if now < (self._snd_queue_last + self._snd_queue_interval):
+            return
+        self._snd_queue_last = now
+        if len(self._snd_queue) > 0:
+            logger.info("Sending queued APRS message")
+            self.send_aprs_data(self._snd_queue.pop(0))
+
+    def on_loop_hook(self):
+        self._dequeue_aprs()
+
+
+def is_br_callsign(callsign):
+    return bool(re.match("P[PTUY][0-9].+", callsign.upper()))
+
+
+class ReplyBot(AprsBot):
+    CONFIG_FILE_NAME = "aprsbot.conf"
+
+    def __init__(self, callsign, host, port):
+        AprsBot.__init__(self, callsign, host, port)
+        self._last_blns = time.monotonic()
+        self._cfg = configparser.ConfigParser()
+        self._config_mtime = None
+        self._check_updated_config()
+
+    def _load_config(self):
+        try:
+            self._cfg.clear()
+            self._cfg.read(ReplyBot.CONFIG_FILE_NAME)
+        except Exception as exc:
+            logger.error(exc)
+
+    def _check_updated_config(self):
+        try:
+            mtime = os.stat(ReplyBot.CONFIG_FILE_NAME).st_mtime
+            if self._config_mtime != mtime:
+                self._load_config()
+                self._config_mtime = mtime
+                logger.info("Configuration reloaded")
+        except Exception as exc:
+            logger.error(exc)
+
+    def on_connect(self):
+        logger.info("Connected")
+
+    def on_disconnect(self):
+        logger.warning("Disconnected! Connecting again...")
+        self.connect()
+
+    def on_recv_frame(self, frame):
+        if len(frame.info) == 0:
+            # No data.
+            return
+
+        source = frame.source.to_string()
+        data_str = frame.info.decode("utf-8", errors="replace")
+
+        if data_str[0] == ":":
+            # Got a direct APRS text message.
+            self.handle_aprs_msg(source, data_str)
+
+        elif data_str[0] == "}":
+            # Got a third-party message, check the payload.
+            # DEBUG RECV: PP5ITT-10>APDW15,PP5JRS-15*,WIDE2-1:}PP5ITT-7>APDR15,TCPIP,PP5ITT-10*::PP5ITT-10:ping 00:01{17
+            via = source
+            src_rest = data_str[1:].split(">", 1)
+            if len(src_rest) != 2:
+                # Unexpected format, nothing useful.
+                return
+            source = src_rest[0]
+            path_msg = src_rest[1].split(":", 1)
+            if len(path_msg) != 2:
+                # No payload.
+                return
+            inner_msg = path_msg[1]
+            if len(inner_msg) > 1 and inner_msg[0] == ":":
+                logger.info("Got a third-party message via %s..." % via)
+                self.handle_aprs_msg(source, inner_msg)
+
+    def handle_aprs_msg(self, source, data_str):
+
+        dest_txt = data_str[1:].split(":", 1)
+        if len(dest_txt) != 2:
+            # Should be a destination station : message
+            return
+
+        msg_sent_to = dest_txt[0].strip()
+        if msg_sent_to.upper() != self.callsign.upper():
+            # This message was not sent for us.
+            return
+
+        text_msgid = dest_txt[1].rsplit("{", 1)
+        text = text_msgid[0]
+        if len(text_msgid) == 2:
+            # This message is asking for an ack.
+            msgid = text_msgid[1]
+        else:
+            msgid = None
+
+        logger.info("Message from %s: %s", source, text)
+
+        qry_args = text.lstrip().split(" ", 1)
+        qry = qry_args[0].lower()
+        args = ""
+        if len(qry_args) == 2:
+            args = qry_args[1]
+
+        if qry == "ping":
+            self.send_aprs_msg(source, "Pong! " + args)
+        elif qry == "version":
+            self.send_aprs_msg(source, "Python " + sys.version.replace("\n", " "))
+        elif qry == "time":
+            self.send_aprs_msg(
+                source, "Localtime is " + time.strftime("%Y-%m-%d %H:%M:%S UTC%Z")
+            )
+        elif qry == "help":
+            self.send_aprs_msg(source, "Valid commands: ping, version, time, help")
+        else:
+            if is_br_callsign(source):
+                self.send_aprs_msg(
+                    source, "Sou um bot. Envie 'help' para a lista de comandos"
+                )
+            else:
+                self.send_aprs_msg(source, "I'm a bot. Send 'help' for command list")
+
+        if msgid:
+            logger.info("Sending ack...")
+            self.send_aprs_msg(source, "ack" + msgid)
+
+    def send_aprs_msg(self, to_call, text):
+        data = ":" + to_call.ljust(9, " ") + ":" + text
+        self.enqueue_aprs_data(data.encode("utf-8"))
+
+    def _update_bulletins(self):
+        if not self._cfg.has_section("bulletins"):
+            return
+
+        max_age = self._cfg.getint("bulletins", "send_freq", fallback=600)
+        now_mono = time.monotonic()
+        if now_mono < (self._last_blns + max_age):
+            return
+
+        self._last_blns = now_mono
+        logger.info("Bulletins are due for update (every %s seconds)", max_age)
+
+        # Bulletins have names in format BLNx, we should send them in
+        # alfabetical order.
+        blns_to_send = []
+        keys = self._cfg.options("bulletins")
+        keys.sort()
+        for key in keys:
+            bname = key.upper()
+            if len(bname) == 4 and bname.startswith("BLN"):
+                blns_to_send.append((bname, self._cfg.get("bulletins", key)))
+
+        # TODO: any post-processing here?
+        for (bln, text) in blns_to_send:
+            self.send_aprs_msg(bln, text)
+
+    def on_loop_hook(self):
+        AprsBot.on_loop_hook(self)
+        self._check_updated_config()
+        self._update_bulletins()
+
+
+if __name__ == "__main__":
+    b = ReplyBot("XX0ABC-10", "192.168.100.40", 8001)
+    b.connect()
+    b.loop()
---
+++ ax25.py
@@ -0,0 +1,272 @@
+"""
+Random utilities for handling AX25 frames
+"""
+
+
+_ADDR_DIGIPEATED_BIT = 0b10000000
+_ADDR_END_OF_PATH_BIT = 1
+
+APRS_CONTROL_FLD = 0x03
+APRS_PROTOCOL_ID = 0xF0
+
+
+def pack_address(callsign, ssid=0, digipeated=False, end_of_path=False):
+    if ssid < 0 or ssid > 15:
+        raise ValueError("Bad SSID %d" % ssid)
+
+    if len(callsign) > 6:
+        raise ValueError("Callsign '%s' is too long" % callsign)
+
+    addr = [a << 1 for a in callsign.ljust(6).encode("ASCII")]
+
+    # Last byte format: h11sssse
+    #   h = Has been digipeated;
+    #   1 = Always one (reserved);
+    #   s = SSID bits;
+    #   e = End of repeater path.
+
+    lastb = 0b01100000 | (ssid << 1)
+    if digipeated:
+        lastb |= _ADDR_DIGIPEATED_BIT
+    if end_of_path:
+        lastb |= _ADDR_END_OF_PATH_BIT
+    addr.append(lastb)
+    return bytes(addr)
+
+
+def parse_address_string(addr_str):
+    """Parse an AX25 address string into its components.
+
+    The following formats are valid:
+
+        PP5ITT      -- Callsign with no SSID;
+        PP5ITT*     -- Digipeated callsign with no SSID;
+        PP5ITT-10   -- Callsign with SSID;
+        PP5ITT-10*  -- Digipeated callsign with SSID.
+    """
+    digipeated = False
+    ssid = 0
+    if addr_str[-1] == "*":
+        digipeated = True
+        addr_str = addr_str[:-1]
+    lst = addr_str.split("-", 1)
+    if len(lst) == 2:
+        ssid = int(lst[1])
+    return lst[0], ssid, digipeated
+
+
+def pack_address_string(addr_str, end_of_path=False):
+    """Pack an AX25 address string to a binary address.
+
+    The following formats are valid:
+
+        PP5ITT      -- Callsign with no SSID;
+        PP5ITT*     -- Digipeated callsign with no SSID;
+        PP5ITT-10   -- Callsign with SSID;
+        PP5ITT-10*  -- Digipeated callsign with SSID.
+    """
+
+    callsign, ssid, digipeated = parse_address_string(addr_str)
+    return pack_address(callsign, ssid, digipeated, end_of_path)
+
+
+def unpack_address(addr):
+    if len(addr) != 7:
+        raise ValueError("Bad AX25 address")
+
+    callsign = "".join(chr(n >> 1) for n in addr[0:6]).strip()
+    ssid = (addr[6] & 0b00011110) >> 1
+    digipeated = bool(addr[6] & _ADDR_DIGIPEATED_BIT)
+    end_of_path = bool(addr[6] & _ADDR_END_OF_PATH_BIT)
+
+    return callsign, ssid, digipeated, end_of_path
+
+
+def format_address_to_string(callsign, ssid, digipeated):
+    cs_pair = callsign
+    if ssid != 0:
+        cs_pair += "-%d" % (ssid)
+    if digipeated:
+        cs_pair += "*"
+    return cs_pair
+
+
+def unpack_address_to_string(addr):
+    callsign, ssid, digipeated, _ = unpack_address(addr)
+    return format_address_to_string(callsign, ssid, digipeated)
+
+
+class Address:
+    def __init__(self, callsign, ssid=0, digipeated=False, end_of_path=False):
+        self.callsign = callsign
+        self.ssid = ssid
+        self.digipeated = digipeated
+        self.end_of_path = end_of_path
+
+    @staticmethod
+    def from_bytes(addr):
+        callsign, ssid, digipeated, end_of_path = unpack_address(addr)
+        return Address(callsign, ssid, digipeated, end_of_path)
+
+    @staticmethod
+    def from_string(addr_str, end_of_path=False):
+        callsign, ssid, digipeated = parse_address_string(addr_str)
+        return Address(callsign, ssid, digipeated, end_of_path)
+
+    def to_bytes(self):
+        return pack_address(self.callsign, self.ssid, self.digipeated, self.end_of_path)
+
+    def to_string(self):
+        return format_address_to_string(self.callsign, self.ssid, self.digipeated)
+
+    def __bytes__(self):
+        return self.to_bytes()
+
+    def __repr__(self):
+        return self.to_string()
+
+    def __str__(self):
+        return self.to_string()
+
+
+def pack_path(addr_strings):
+
+    return b"".join(
+        pack_address_string(a, False) for a in addr_strings[:-1]
+    ) + pack_address_string(addr_strings[-1], True)
+
+
+def unpack_path(path):
+
+    if len(path) % 7 != 0:
+        # It's an error: addresses are always 7 bytes long.
+        raise ValueError("Invalid path length")
+
+    return [unpack_address_to_string(path[i : i + 7]) for i in range(0, len(path), 7)]
+
+
+def unpack_path_to_addrs(path):
+
+    if len(path) % 7 != 0:
+        # It's an error: addresses are always 7 bytes long.
+        raise ValueError("Invalid path length")
+
+    return [Address.from_bytes(path[i : i + 7]) for i in range(0, len(path), 7)]
+
+
+class Frame:
+    def __init__(self, source, dest, path, control, pid, info):
+        self.source = source
+        self.dest = dest
+        self.path = path
+        self.control = control
+        self.pid = pid
+        self.info = info
+
+    @staticmethod
+    def from_kiss_bytes(fdata):
+        pos = 0
+        dlen = len(fdata)
+        if dlen < 19:
+            raise ValueError(
+                "Frame length is smaller than expected minimum. frame data: "
+                + frame.hex()
+            )
+
+        dest = Address.from_bytes(fdata[0:7])
+        pos += 7
+
+        addr_list = []
+        while pos < dlen - 7:
+            addr_list.append(Address.from_bytes(fdata[pos : pos + 7]))
+            pos += 7
+            if addr_list[-1].end_of_path:
+                break
+
+        source = addr_list[0]
+        path = addr_list[1:]
+
+        if pos >= dlen - 2:
+            raise ValueError("Invalid frame data: " + fdata.hex())
+
+        control = fdata[pos]
+        pid = fdata[pos + 1]
+        info = fdata[pos + 2 :]
+
+        return Frame(source, dest, path, control, pid, info)
+
+    def _update_end_of_path_flags(self):
+        """Ensure "end of path" information is always valid.
+        """
+        self.source.end_of_path = False
+        if len(self.path) > 0:
+            self.dest.end_of_path = False
+            for p in self.path:
+                p.end_of_path = False
+            self.path[-1].end_of_path = True
+        else:
+            self.dest.end_of_path = True
+
+    def to_kiss_bytes(self):
+        self._update_end_of_path_flags()
+        return (
+            self.dest.to_bytes()
+            + self.source.to_bytes()
+            + b"".join(p.to_bytes() for p in self.path)
+            + bytes([self.control, self.pid])
+            + self.info
+        )
+
+    @staticmethod
+    def from_aprs_string(frame_str):
+        """frame_str is a *bytes* object!  Rename this.
+        """
+
+        # PP5ITT-7>APDR15,PP5JRS-15*,WIDE2-2,qAR,PU5BRA-10:=2628.97S/04906.81Wx Ittner
+
+        # Split the frame in headers and data
+        lst = frame_str.split(b":", 1)
+        if len(lst) != 2:
+            raise ValueError("Bad APRS frame string")
+
+        # Headers must be ASCII. Otherwhise is an error.
+        headers = lst[0].decode("ascii")
+        info = lst[1]
+
+        lst = headers.split(">", 1)
+        if len(lst) != 2:
+            raise ValueError("Bad headers in APRS frame string")
+
+        source = Address.from_string(lst[0])
+        addrs = [Address.from_string(s) for s in lst[1].split(",")]
+        if len(addrs) == 0:
+            raise ValueError("No destination address in APRS frame string")
+
+        dest = addrs[0]
+        addrs[-1].end_of_path = True
+        path = addrs[1:]
+
+        f = Frame(source, dest, path, APRS_CONTROL_FLD, APRS_PROTOCOL_ID, info)
+        f._update_end_of_path_flags()
+        return f
+
+    def to_aprs_string(self):
+        """Convert the frame to a APRS string. Does not suport Mic-E yet.
+
+        The APRS string is actually a *byte* string, despite the lion's
+        share of the messages being ASCII-only.
+        """
+
+        buf = (
+            self.source.to_string().encode("ASCII")
+            + b">"
+            + self.dest.to_string().encode("ASCII")
+        )
+        if len(self.path) > 0:
+            buf += b"," + b",".join(a.to_string().encode("ASCII") for a in self.path)
+        buf = buf + b":" + self.info
+
+        return buf
+
+    def __repr__(self):
+        return self.to_aprs_string().decode("utf-8", errors="replace")
